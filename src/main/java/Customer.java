dd0bcc0a61a694b21c134eddddbeb3f5d647e4da
uHirsch <66682042+Swr96@users.noreply.github.com>
2022-05-11 15:55:05 +0200

    added querry method

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -21,23 +21,33 @@ public class Customer {
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
-            thisAmount = each.getCharge();
             // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints ++;
+            frequentRenterPoints += each.getFrequentRenterPoints();
+
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
+
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " +
+                String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints)
+                +
+                " frequent renter points";
+        return result;
+
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration enum_rentals = rentals.elements();
+        while (enum_rentals.hasMoreElements()) {
+            Rental each = (Rental) enum_rentals.nextElement();
+            result += each.getCharge();
+        }
         return result;
     }
+
 
 }
