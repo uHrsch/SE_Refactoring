25f86d65a5dd563800fc1bcfa002881d54858c7e
uHirsch <66682042+Swr96@users.noreply.github.com>
2022-05-11 17:41:20 +0200

    resolve code smells in CustomerTest class

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -5,18 +5,18 @@ import static org.junit.Assert.assertEquals;
 import java.util.Vector;
 
 
-public class CustomerTest {
+class CustomerTest {
 
     static final Customer customer = new Customer("Adam");
     static final Vector rentals = new Vector();
 
     @Test
-    public void getName() {
+    void getName() {
         assertEquals("Adam", this.customer.getName());
     }
 
     @Test
-    public void statement() {
+    void statement() {
         String statementExp = "Rental Record for " + customer.getName() + "\n";
         statementExp += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
         statementExp += "Amount owed is " + "0.0" + "\n";
@@ -26,7 +26,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void addRental() {
+    void addRental() {
         int i = rentals.size();
 
         rentals.addElement(new Rental(new Movie("TITLE", 0), 0));
